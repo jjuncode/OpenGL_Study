ee27f7a13412c584a532eb5eab971aa83056001d
jjuncode <jjun.code@gmail.com>
2023-08-20 01:26:45 +0900

    fix
    함수 정의, 선언 구분

--- main.cpp
+++ main.cpp
@@ -6,25 +6,8 @@ const int WINDOW_WIDTH{ 800 };
 const int WINDOW_HEIGHT{ 800 };
 const char* const WINDOW_NAME{ "Test" };
 
-void OnKeyEvent(GLFWwindow* window,
-    int key, int scancode, int action, int mods) {
-    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
-        key, scancode,
-        action == GLFW_PRESS ? "Pressed" :
-        action == GLFW_RELEASE ? "Released" :
-        action == GLFW_REPEAT ? "Repeat" : "Unknown",
-        mods & GLFW_MOD_CONTROL ? "C" : "-",
-        mods & GLFW_MOD_SHIFT ? "S" : "-",
-        mods & GLFW_MOD_ALT ? "A" : "-");
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
-
-void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
-    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
-    glViewport(0, 0, width, height);
-}
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods);
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height);
 
 int main(int argc, const char** argv) {
     // 시작을 알리는 로그
@@ -76,4 +59,24 @@ int main(int argc, const char** argv) {
 
     glfwTerminate();
     return 0;
+}
+
+void OnKeyEvent(GLFWwindow* window,
+    int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    glViewport(0, 0, width, height);
 }
